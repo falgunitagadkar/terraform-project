8339311ec89efe6f8f14331990873fefb883f63b
Falguni <falgunitagadkar@gmail.com>
2025-10-25 19:11:18 +0530

    Removed comments

--- app/index.js
+++ app/index.js
@@ -1,11 +1,8 @@
-// Import express
 const express = require('express');
 const app = express();
 const port = 8080;//port defined here
 
-// Define routes
 app.get('/', (req, res) => res.send('Hello, App running on Version 1.0'));
 app.get('/health', (req, res) => res.send('OK'));
 
-// Start server
 app.listen(port, () => console.log(`App running on port ${port}`));
